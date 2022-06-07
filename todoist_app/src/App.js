f346ac2335659681e5ed391872a819e641f3517e
Abhishek Dhar <abhishek473@gmail.com>
2022-06-07 16:34:43 -0400

    Set up app page

--- todoist_app/src/App.js
+++ todoist_app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/tasks").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(typeof(data.tasks) === 'undefined') ? (
+        <p> Loading...</p>
+      ) : (
+        data.tasks.map((task, i) => (
+          <p key={i}>{task}</p>
+        ))
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
