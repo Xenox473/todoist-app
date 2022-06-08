19f12c292dc07abb22cac1f16d6095bc19ef9dc4
Abhishek Dhar <abhishek473@gmail.com>
2022-06-08 15:10:20 -0400

    Fetch habits in server

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,26 @@
 from flask import Flask
+import todoist
+import os
 
 #initialize app
 app = Flask(__name__)
 
+def get_habits(api, page=4):
+    print("getting habits")
+    completed_activities = api.activity.get(event_type = "completed")
+    habit_label = list(filter(lambda x: x['name'] == 'Habit', api.labels.all()))[0]
+    habits = list(map(lambda x: x['content'], list(filter(lambda x: habit_label['id'] in x['labels'], api.items.all()))))
+
+    return habits
+
 #Tasks API
 @app.route('/tasks', methods=['GET'])
 def tasks():
-    return {"tasks": ["Task1", "Task2", "Task3"]}
+    api_key = os.environ.get('TODOIST_API_KEY')
+    api = todoist.TodoistAPI(api_key)
+    api.sync()
+    habits = get_habits(api)
+    return {"tasks": habits}
 
 if __name__ == '__main__':
     app.run(debug=True)
