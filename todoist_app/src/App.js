432ad57b125d70d6ec71509ed3c24083fc226f40
Abhishek Dhar <abhishek473@gmail.com>
2022-06-08 15:32:32 -0400

    Added linter

--- todoist_app/src/App.js
+++ todoist_app/src/App.js
@@ -1,30 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
-function App() {
-
-  const [data, setData] = useState([{}])
+const App = () => {
+  const [data, setData] = useState([{}]);
 
   useEffect(() => {
-    fetch("/tasks").then(
-      res => res.json()
+    fetch('/tasks').then(
+      (res) => res.json(),
     ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-  return (
-    <div>
-      {(typeof(data.tasks) === 'undefined') ? (
-        <p> Loading...</p>
-      ) : (
-        data.tasks.map((task, i) => (
-          <p key={i}>{task}</p>
-        ))
-      )}
-    </div>
-  )
-}
+      (response) => {
+        setData(response);
+        console.log(response);
+      },
+    );
+  }, []);
 
-export default App
+  // if (typeof(data?.tasks) === 'undefined') return <p>Loading...</p>;
+
+  return data?.tasks?.map((task) => (
+    <p key={task}>{task}</p>
+  ));
+};
+
+export default App;
